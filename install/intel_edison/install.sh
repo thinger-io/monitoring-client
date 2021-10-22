348d46c9d74f6a9d918ba0988780f2679929d858
BautisFlow <jaimebs94@gmail.com>
2021-10-22 12:54:32 +0200

    Removed intel and raspberry install files and added monitor service

--- install/intel_edison/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-mkdir -p build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DDAEMON=ON -DEDISON=ON ../../../
-systemctl stop thinger.service
-make thinger
-make install
-systemctl start thinger.service
-systemctl enable thinger.service
